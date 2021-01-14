04ec72875e33703a92cba838774ee4fc9ccb75e6
Wilfredo R Colina <wrca26@gmail.com>
2021-01-14 17:42:03 -0600

    changes on about pages implemented, need work

--- wildlife-response/src/components/About/About.js
+++ wildlife-response/src/components/About/About.js
@@ -5,10 +5,11 @@ import dotImage from "../../images/dots-wildlifeWebsite.png";
 import Subtitle from "../Subtitle/Subtitle";
 import DirectorSquare from "./DirectorSquare/DirectorSquare";
 import classes from "./About.module.css";
+import Responders from "./Responders/Responders";
 const About =()=>{
 return(
     <>
-            <section style={{backgroundImage:`url(${dotImage})`, backgroundRepeat:"repeat-x "}}>s
+            <section style={{backgroundImage:`url(${dotImage})`, backgroundRepeat:"repeat"}}>s
                     <LogoNav/>
                     <NavTwo/>
                     <Subtitle titleName={"About us"} />
@@ -17,6 +18,7 @@ return(
                     Wildlife Response Services, LLC has a number of uniquely qualified wildlife responders and maintains a database of additional on-call contract personnel to assist with an incident, regardless of size or scope
                     The company was established in 2006 and is in the process of completing documentation for certification as both a Woman-Owned and Historically Underutilized Business.    </p>
                     <DirectorSquare/>
+                    <Responders/>
             </section>
 
         </>
---
+++ wildlife-response/src/components/About/Responders/Responders.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import classes from "./Responders.module.css";
+import animal1 from "../../../images/animal1.jpg";
+import animal2 from "../../../images/animal2.jpg";
+import animal3 from "../../../images/animal3.jpg";
+const Responders =()=>{
+    return(
+        <>
+            <h2 className={classes.positionTitle}>Deer</h2>
+        <div className={classes.container}>
+            <div className={classes.picture}> <img src={animal1} alt="william " className={classes.image}/> </div>
+            <div className={classes. information}>
+                <p className={classes.description}> Brian Cain received a Bachelor and Master of Science degree from Texas A&I Univ. in Kingsville, TX in 1965 and 1968,
+                and a Ph D from the Univ. of Illinois in 1972. He worked as an Assistant Professor at Texas A&M University in the Wildlife Department until 1978 when
+                he joined the U. S.
+                Fish and Wildlife Service as a Contaminant Specialist. He remained in that position until retiring in January 2007.</p> </div>
+            <div/>
+        </div>
+            <h2 className={classes.positionTitleTwo}>Panda</h2>
+            <div className={classes.containerRight}>
+
+                <div className={classes.pictureTwo}>
+                    <img src={animal2} alt="william" className={classes.image}/> </div>
+                <div className={classes. informationTwo}>
+                    <p className={classes.description}> Brian Cain received a Bachelor and Master of Science degree from Texas A&I Univ. in Kingsville, TX in 1965 and 1968,
+                        and a Ph D from the Univ. of Illinois in 1972. He worked as an Assistant Professor at Texas A&M University in the Wildlife Department until 1978 when
+                        he joined the U. S.
+                        Fish and Wildlife Service as a Contaminant Specialist. He remained in that position until retiring in January 2007.</p> </div>
+                <div/>
+            </div>
+            <h2 className={classes.positionTitle}>Turtle</h2>
+            <div className={classes.container}>
+                <div className={classes.picture}> <img src={animal3} alt="william " className={classes.image}/> </div>
+                <div className={classes. information}>
+                    <p className={classes.description}> Brian Cain received a Bachelor and Master of Science degree from Texas A&I Univ. in Kingsville, TX in 1965 and 1968,
+                        and a Ph D from the Univ. of Illinois in 1972. He worked as an Assistant Professor at Texas A&M University in the Wildlife Department until 1978 when
+                        he joined the U. S.
+                        Fish and Wildlife Service as a Contaminant Specialist. He remained in that position until retiring in January 2007.</p> </div>
+                <div/>
+            </div>
+
+        </>
+    )
+};
+export default Responders;
