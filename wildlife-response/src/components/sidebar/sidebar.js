93d91a49e7154db626b7a0ad0367d0e753ce8f21
Wilfredo R Colina <wrca26@gmail.com>
2021-01-28 16:25:49 -0600

    changes on sidebar for the home page

--- wildlife-response/src/components/sidebar/sidebar.js
+++ wildlife-response/src/components/sidebar/sidebar.js
@@ -2,42 +2,50 @@ import React, {useState} from "react";
 import {Link} from "gatsby";
 import classes from "./sidebar.module.css";
 import {IoIosMenu, IoMdClose} from "react-icons/io";
+import Backdrop from "../Backdrop/Backdrop";
 const Sidebar=()=>{
+    let attachedClasses = [classes.sidebarWrapper, classes.Close];
+    if (!toggleSidebar) {
+        attachedClasses = [classes.sidebarWrapper, classes.Open];
+    }
     let [toggleSidebar, setToggleSidebar]=useState(false);
     const NavLink=(Link);
     const onToggle=()=>{
       setToggleSidebar(!toggleSidebar);
     };
-    let navigation=null
-    let icon = <IoIosMenu  onClick={onToggle} className='icon-2' style={{
+    let conditionalNav=<IoIosMenu  onClick={onToggle} className='icon-2' style={{
         position:"relative",
         top:"7px",
         fontSize:"40px",
         color:"#14281f",
         marginRight:"20px"
     }}/>
-    if(toggleSidebar===true){
 
-        icon= <IoMdClose onClick={onToggle} className='icon-2' style={{
+    if(toggleSidebar===true){
+        conditionalNav=<>
+            <Backdrop show={toggleSidebar}/>
+            <div className={attachedClasses.join(' ')}>
+            <IoMdClose onClick={onToggle} className='icon-2' style={{
             position:"relative",
             top:"7px",
             fontSize:"40px",
             color:"#14281f",
             marginRight:"20px"
-        }}/>
-
-        navigation=  <nav className={classes.itemsSidebar} >
+        }}/> <nav className={classes.itemsSidebar} >
             <NavLink to='/services' className={classes.itemSidebar} >Services</NavLink>
             <NavLink to='/mission' className={classes.itemSidebar}>Mission</NavLink>
             <NavLink to='/about' className={classes.itemSidebar}>About</NavLink>
             <NavLink to='/contact' className={classes.itemSidebar}>Contact</NavLink>
         </nav>
+            </div>
+        </>
+
+
     }
     return(
         <>
             <div className={classes.navIcon}  >
-                {icon}
-                {navigation}
+                {conditionalNav}
             </div >
             </>
     )
