76456134af7a02226279f7b319deb851510ae900
Wilfredo R Colina <wrca26@gmail.com>
2021-01-29 22:34:36 -0600

    Animation on Sidebar applied and funtionality improved

--- wildlife-response/src/components/sidebar/sidebar.js
+++ wildlife-response/src/components/sidebar/sidebar.js
@@ -1,51 +1,56 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import {Link} from "gatsby";
 import classes from "./sidebar.module.css";
 import {IoIosMenu, IoMdClose} from "react-icons/io";
 import Backdrop from "../Backdrop/Backdrop";
 const Sidebar=()=>{
-    let attachedClasses = [classes.sidebarWrapper, classes.Close];
-    if (!toggleSidebar) {
-        attachedClasses = [classes.sidebarWrapper, classes.Open];
-    }
-    let [toggleSidebar, setToggleSidebar]=useState(false);
+    let [toggleSidebar, setToggleSidebar]=useState("closed");
+    let attachedClasses=[classes.sidebarWrapper];
+
     const NavLink=(Link);
-    const onToggle=()=>{
-      setToggleSidebar(!toggleSidebar);
+    const openSidebar=()=>{
+      setToggleSidebar("open");
     };
-    let conditionalNav=<IoIosMenu  onClick={onToggle} className='icon-2' style={{
-        position:"relative",
-        top:"7px",
-        fontSize:"40px",
-        color:"#14281f",
-        marginRight:"20px"
-    }}/>
-
-    if(toggleSidebar===true){
-        conditionalNav=<>
-            <Backdrop show={toggleSidebar}/>
-            <div className={attachedClasses.join(' ')}>
-            <IoMdClose onClick={onToggle} className='icon-2' style={{
-            position:"relative",
-            top:"7px",
-            fontSize:"40px",
-            color:"#14281f",
-            marginRight:"20px"
-        }}/> <nav className={classes.itemsSidebar} >
-            <NavLink to='/services' className={classes.itemSidebar} >Services</NavLink>
-            <NavLink to='/mission' className={classes.itemSidebar}>Mission</NavLink>
-            <NavLink to='/about' className={classes.itemSidebar}>About</NavLink>
-            <NavLink to='/contact' className={classes.itemSidebar}>Contact</NavLink>
-        </nav>
-            </div>
-        </>
-
-
+    const closeSidebar = ()=>{
+      setToggleSidebar("closing")
+    };
+    if ( toggleSidebar==="closed"){
+        attachedClasses=[classes.sidebarWrapper, classes.Closed];
     }
+    if (toggleSidebar==="open") {
+        attachedClasses = [classes.sidebarWrapper, classes.Open];
+    }else if (toggleSidebar==="closing"){
+        attachedClasses = [classes.sidebarWrapper, classes.Close];
+    }
+
     return(
         <>
             <div className={classes.navIcon}  >
-                {conditionalNav}
+                <>
+                    <IoIosMenu  onClick={openSidebar} className='icon-2' style={{
+                        position:"relative",
+                        top:"7px",
+                        fontSize:"40px",
+                        color:"#14281f",
+                        marginRight:"20px"
+                    }}/>
+                    <Backdrop show={toggleSidebar==="open" ?true: false } />
+                    <div className={attachedClasses.join(' ')} >
+
+                        <IoMdClose onClick={closeSidebar} className='icon-2' style={{
+                            position:"relative",
+                            top:"7px",
+                            fontSize:"40px",
+                            color:"#14281f",
+                            marginRight:"20px"
+                        }}/> <nav className={classes.itemsSidebar} >
+                        <NavLink to='/services' className={classes.itemSidebar} >Services</NavLink>
+                        <NavLink to='/mission' className={classes.itemSidebar}>Mission</NavLink>
+                        <NavLink to='/about' className={classes.itemSidebar}>About</NavLink>
+                        <NavLink to='/contact' className={classes.itemSidebar}>Contact</NavLink>
+                    </nav>
+                    </div>
+                </>
             </div >
             </>
     )
