32ca23f0f67cabc2e75aa79839a7d4929bad704d
Wilfredo R Colina <wrca26@gmail.com>
2021-02-19 16:51:41 -0600

    fixing folder structure and navbar

---
+++ src/components/Home/NavBar/NavBar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import {Link} from "gatsby";
+import classes from "./Navbar.module.css"
+import logo from "../../../images/wil2.png";
+import Sidebar from "../../sidebar/sidebar";
+const NavBar = () =>{
+    const NavLink =(Link);
+
+    return(
+        <>
+            <div className="container-fluid">
+                <div className="row">
+            <div className="col-6">
+                <NavLink to='/' > <img src={logo} alt="wildlife-logo" className={classes.image}/> </NavLink>
+            </div>
+                <div className="col-6">
+                    <div className={classes.sidebar}> <Sidebar/> </div>
+                    <nav className={classes.parent} >
+
+                            <NavLink to='/services' className={classes.children} >Services</NavLink>
+                            <NavLink to='/mission' className={classes.children}>Mission</NavLink>
+                            <NavLink to='/about' className={classes.children}>About</NavLink>
+                            <NavLink to='/contact' className={classes.children}>Contact us</NavLink>
+
+                    </nav>
+
+                    </div>
+                </div>
+            </div>
+
+        </>
+    )
+};
+export default NavBar;
