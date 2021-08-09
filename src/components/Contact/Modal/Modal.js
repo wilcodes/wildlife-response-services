1dae762a4ebfa9499d437ad0d9316a41919a7020
Wilfredo R Colina <wrca26@gmail.com>
2021-08-09 12:30:47 -0500

    adding home link in the modal component

--- src/components/Contact/Modal/Modal.js
+++ src/components/Contact/Modal/Modal.js
@@ -3,8 +3,10 @@ import Backdrop from "../../Backdrop/Backdrop";
 import classes from "./Modal.module.css";
 import { TiThumbsDown, TiThumbsUp } from "react-icons/ti";
 import Spinner from "../Spinner/Spinner";
+import {Link} from "gatsby";
+import logo from "../../../images/white-logo.png"
 const Modal = (props)=> {
-
+  const Navlink=(Link);
   let modal = null;
   if (props.message ==="open") {
   modal =  <div className={classes.Modal}>
@@ -13,6 +15,9 @@ const Modal = (props)=> {
       <TiThumbsUp className={classes.icons}/>
     </p>
     <p className={classes.subDescription}> Wildlife Response Services Team </p>
+    <p className={classes.modalLink}>
+      <Navlink to={"/"}  className={classes.modalLink}>Home &rarr;</Navlink>
+    </p>
 
   </div>
   }
@@ -23,7 +28,12 @@ const Modal = (props)=> {
     modal = <>
       <div className={classes.Modal}>
       <p className={classes.subDescription}> Your Message could not be sent </p>
-      <p className={classes.description}> Please Reload the page and try Again <TiThumbsDown className={classes.icons}/></p>
+      <p className={classes.description}> Please Reload the page and try Again
+        <TiThumbsDown className={classes.icons}/>
+      </p>
+        <p className={classes.modalLink}>
+          <Navlink to={"/"}  className={classes.modalLink}>Home &rarr;</Navlink>
+        </p>
 
     </div>
   </>
